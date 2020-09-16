00e508ebc6e1b6fda8d5f497f3b94297a55d36d7
santiteeragul <santitee@gmai.com>
2020-09-16 09:30:43 +0700

    Flutter Food Delivery Application Recent Order and Near by Restaurants

---
+++ lib/models/user.dart
@@ -0,0 +1,19 @@
+
+
+
+
+import 'package:flutter_food_delivery/models/order.dart';
+
+import 'order.dart';
+
+class User {
+  final String name;
+  final List<Order> orders;
+  final List<Order> cart;
+
+  User({
+    this.name,
+    this.orders,
+    this.cart,
+  });
+}
---
+++ lib/widgets/rating_stars.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+
+class RatingStars extends StatelessWidget {
+
+  final int rating;
+
+  RatingStars(this.rating);
+
+  @override
+  Widget build(BuildContext context) {
+    String stars = '';
+    for (int i = 0; i < rating; i++) {
+      stars += '⭐️  ';
+    }
+    stars.trim();
+    return Text(
+      stars,
+      style: TextStyle(
+        fontSize: 18.0,
+      ),
+    );
+  }
+}
